764dc7bcc7bb31949f03baf9ea3c8042c0f375ac
Marc Bastian Heinrichs <typo3@mbh-software.de>
2019-01-27 00:13:30 +0100

    [FEATURE] Add extension code

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Redirects',
+    'description' => 'Manage redirects for your TYPO3-based website.',
+    'category' => 'fe',
+    'author' => 'TYPO3 Core Team',
+    'author_email' => 'typo3cms@typo3.org',
+    'author_company' => '',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '8.7.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-8.7.99'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
